dda84823f699f9056031dcc03a7351b8267d5d1a
Tom Bartle <1022049@student.sae.edu.au>
2021-08-03 12:42:45 +1000

    Player moving and jumping

---
+++ Assets/Tom/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 5f;
+
+    private float horizontal;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+}
