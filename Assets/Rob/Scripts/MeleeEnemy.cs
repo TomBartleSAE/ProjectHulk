267756c77e336bbe5afeb9e5d5ec9b4d6ddbc40f
Tom Bartle <1022049@student.sae.edu.au>
2021-08-22 16:20:11 +1000

    Added Ultron with animations

--- Assets/Rob/Scripts/MeleeEnemy.cs
+++ Assets/Rob/Scripts/MeleeEnemy.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Tom;
 using UnityEngine;
 
 public class MeleeEnemy : Enemy
@@ -10,31 +12,52 @@ public class MeleeEnemy : Enemy
 
     public float meleeAttackSpeed;
 
-   
+    private Animator anim;
+
+    public override void Start()
+    {
+        base.Start();
+
+        anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     private void Update()
     {
         if (player != null)
         {
-            if(Vector2.Distance(transform.position, player.position)> stopDistance)
+            if (Vector2.Distance(transform.position, player.position) > stopDistance)
             {
                 transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+                anim.SetFloat("Movement", 1);
             }
             else
             {
-                if(Time.time >= attackTime)
+                if (Time.time >= attackTime)
                 {
                     StartCoroutine(MeleeAttack());
                     attackTime = Time.time + timeBetweenAttacks;
                 }
+
+                anim.SetFloat("Movement", 0);
+            }
+            
+            if (player.position.x - transform.position.x > 0)
+            {
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0, transform.eulerAngles.z);
+            }
+
+            if (player.position.x - transform.position.x < 0)
+            {
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, 180, transform.eulerAngles.z);
             }
         }
     }
 
     IEnumerator MeleeAttack()
     {
-        //player.GetComponent <Player>().TakeDamage(damage);
+        player.GetComponent<Health>().TakeDamage();
+        anim.SetTrigger("Attack");
 
         Vector2 originalPosition = transform.position;
         Vector2 targetPosition = player.position;
@@ -46,8 +69,22 @@ public class MeleeEnemy : Enemy
             float formula = (-Mathf.Pow(percent, 2) + percent) * 4;
             transform.position = Vector2.Lerp(originalPosition, targetPosition, formula);
             yield return null;
-
         }
     }
-}
 
+    private void OnEnable()
+    {
+        GetComponent<Health>().OnDamageEvent += Die;
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<Health>().OnDamageEvent -= Die;
+    }
+
+    private void Die()
+    {
+        anim.SetTrigger("Die");
+        Destroy(gameObject, 5);
+    }
+}
