5c506b1aa95f1cdef918fc641e4a59ce41397d3b
Rob <1021549@student.sae.edu.au>
2021-08-23 11:55:45 +1000

    yuppers

--- Assets/Rob/Scripts/PanelScreen.cs
+++ Assets/Rob/Scripts/PanelScreen.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PanelScreen : MonoBehaviour
 {
     public GameObject winPanel;
     public GameObject losePanel;
+    public Button button;
+    
+    
+    void Start () {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(PlayAgain);
+    }
+
 
 
 
@@ -19,6 +28,11 @@ public class PanelScreen : MonoBehaviour
     public void WinGame()
     {
         winPanel.SetActive(true);
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
 }
